94d464637984d1c4b7027eee2f36173d3cfdcfa7
Etrisa <johan.samuelsson42@gmail.com>
2020-02-29 22:32:22 +0100

    Day 1 done!

--- Day1.ps1
+++ Day1.ps1
@@ -6,33 +6,33 @@ $charCount1 = ($input.ToCharArray() | Where-Object {$_ -eq '('} | Measure-Object
 $charCount2 = ($input.ToCharArray() | Where-Object {$_ -eq ')'} | Measure-Object).Count
 #Write-Host $charCount2
 
-$floor = [math]::abs($charCount1 - $charCount2)
-$floor
+$Floor = [math]::abs($charCount1 - $charCount2)
+$Floor
+$Floor = 0
+
+
+$input = $input.ToCharArray()
+
+Foreach ($character in $input){
+  $i += 1
+
+  If ($character -eq '('){
+      $Floor += 1
+  }else{
+      $Floor += -1
+  }
+  If ($floor -lt 0){
+    write-Host $floor
+    break
+  }
+    write-Host $i
+}
+
+
 
 cmd /c pause | out-null
 
-
-
 <#
---- Day 1: Not Quite Lisp ---
-Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars, and he's fresh out! To save Christmas, he needs you to collect fifty stars by December 25th.
-
-Collect stars by helping Santa solve puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
-
-Here's an easy puzzle to warm you up.
-
-Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
-
-An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
-
-The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
-
-For example:
-
-(()) and ()() both result in floor 0.
-((( and (()(()( both result in floor 3.
-))((((( also results in floor 3.
-()) and ))( both result in floor -1 (the first basement level).
-))) and )())()) both result in floor -3.
-To what floor do the instructions take Santa?
+Answer 1: 138
+Answer 2: 1771
 #>
